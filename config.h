42c39c3694067aa745a7023de4d050f39ed04afb
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-07-07 11:41:19 +0800

    change: color
    color

--- config.h
+++ config.h
@@ -5,16 +5,16 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"BerkeleyMono NerdFont:size=10"
+	"BerkeleyMono NerdFont:style=Bold:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel] = { "#839496", "#073642"},
-	[SchemeSelHighlight] = { "#ffc978", "#073642" },
-	[SchemeNormHighlight] = { "#ffc978", "#002b36" },
-	[SchemeOut] = { "#002b36", "#00ffff" },
+	[SchemeNorm] = { "#ffffff", "#1b1b1b" },
+	[SchemeSel] = { "#ffffff", "#005577"},
+	[SchemeSelHighlight] = { "#fee56c", "#005577" },
+	[SchemeNormHighlight] = { "#fee56c", "#1b1b1b" },
+	[SchemeOut] = { "#08e7c5", "#1b1b1b" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
